565da69fd4a672f93fc1f12e4b38c1521a65e2fd
Phicks-debug <an.tq@techxcorp.com>
2025-04-26 15:01:56 +0700

    init commit

---
+++ example/invoke_model.py
@@ -0,0 +1,37 @@
+import json
+import asyncio
+
+from aiobedrock import Client
+
+
+async def main():
+    async with Client(region_name="ap-southeast-1") as client:
+        body = {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 4096,
+            "top_k": 40,
+            "temperature": 0.7,
+            "top_p": 0.9,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "What can you do?"},
+                    ],
+                }
+            ],
+        }
+
+        response = await client.invoke_model(
+            body=json.dumps(body),
+            modelId="anthropic.claude-3-haiku-20240307-v1:0",
+            accept="application/json",
+            contentType="application/json",
+            trace="ENABLED_FULL",
+        )
+
+        print(json.loads(response.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ example/invoke_model_with_response_stream.py
@@ -0,0 +1,36 @@
+import json
+import asyncio
+
+from aiobedrock import Client
+
+
+async def main():
+    async with Client(region_name="ap-southeast-1") as client:
+        body = {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 4096,
+            "top_k": 40,
+            "temperature": 0.7,
+            "top_p": 0.9,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "What can you do?"},
+                    ],
+                }
+            ],
+        }
+
+        async for chunk in client.invoke_model_with_response_stream(
+            body=json.dumps(body),
+            modelId="anthropic.claude-3-haiku-20240307-v1:0",
+            accept="application/json",
+            contentType="application/json",
+            trace="ENABLED_FULL",
+        ):
+            print(json.loads(chunk.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
