81204c82fe58d8da381db79b0f0db7271d4fc705
Phicks-debug <an.tq@techxcorp.com>
2025-04-26 23:32:33 +0700

    updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="aiobedrock",
-    version="0.1.2",
+    version="0.1.3",
     author="Phicks",
     author_email="an.tq@techxcorp.com",
     description="AWS boto3 bedrock client in async",
